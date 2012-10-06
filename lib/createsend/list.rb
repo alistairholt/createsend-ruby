84971434a7f5483cc7e746d112c7075af58240ee
Alistair Holt <hello@alistairholt.co.uk>
2012-10-06 15:43:43 +0100

    Bug fix

--- lib/createsend/list.rb
+++ lib/createsend/list.rb
@@ -103,8 +103,8 @@ module CreateSend
       response = get "active", options
       response = Hashie::Mash.new(response)
       # Sort out CM's shite CustomFields output
-      custom_fields = response.Results.CustomFields
-      response.Results.CustomFields = custom_fields.each_with_object({}) do |field, hsh|
+      custom_fields = response.Results.first.CustomFields
+      response.Results.first.CustomFields = custom_fields.each_with_object({}) do |field, hsh|
         hsh[field.Key] = field.Value
       end
       response
